d9df1251c300f93853bd0d73a966e2e22e267004
MarekDudek <marek.dudek@gmail.com>
2022-10-29 17:36:39 +0200

    JUnit configured

---
+++ src/test/java/md/MainTest.java
@@ -0,0 +1,15 @@
+package md;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MainTest
+{
+
+    @Test
+    void test()
+    {
+        assertThat(2 + 2).isEqualTo(4);
+    }
+}
